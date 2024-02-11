de90c286892ffee89ec57cb8d3f64d76fad286d9
AbdullahPro2 <zeb16208@gmail.com>
2024-02-11 23:56:53 +0100

    CarouselFunctionality Added

--- src/ui/CarouselItem.jsx
+++ src/ui/CarouselItem.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function CarouselItem({ item }) {
   return (
-    <div className="flex min-w-full">
-      <div>
-        <h2>{item.name}</h2>
-        <p>{item.description}</p>
-        <div>
-          <button>Order Now</button>
-        </div>
+    <div className="flex min-w-full justify-between bg-orange-200 px-4 py-8 sm:h-[50vh] sm:px-12">
+      <div className="flex w-[50%] flex-col justify-center ">
+        <h2 className="mb-2 text-xl font-bold ">{item.name}</h2>
+        <p className="text-sm">{item.description}</p>
+        <button className="mt-2 rounded bg-white px-4 py-2 text-black hover:bg-green-200 focus:border-blue-300 focus:outline-none focus:ring">
+          Order Now
+        </button>
       </div>
-      <img src={item.image} className="w-[500px]" alt="pizza of carousel" />
+      <img src={item.image} className="w-52 rounded" alt="pizza of carousel" />
     </div>
   );
 }
