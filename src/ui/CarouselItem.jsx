bbc24119092ae68b83a8f367dedbf5bae87d6ac7
AbdullahPro2 <zeb16208@gmail.com>
2024-02-14 00:39:00 +0100

    Carousel Refactoring

--- src/ui/CarouselItem.jsx
+++ src/ui/CarouselItem.jsx
@@ -3,23 +3,36 @@ import { useNavigate } from 'react-router-dom';
 
 function CarouselItem({ item }) {
   const navigate = useNavigate();
+  // 'flex min-w-full justify-between gap-4 bg-orange-200 px-4 py-8 sm:h-[60vh] sm:px-12 md:justify-center';
   return (
-    <div className="flex min-w-full justify-between gap-4 bg-orange-200 px-4 py-8 sm:h-[60vh] sm:px-12">
-      <div className="flex w-[50%] flex-col justify-center ">
+    <div className=" grid min-w-full grid-cols-2 gap-2 bg-orange-200 px-4 py-8 sm:h-[60vh] sm:px-12">
+      <div className="flex  flex-col justify-center  md:items-end ">
         <h2 className="mb-2 text-xl font-bold sm:text-2xl ">{item.name}</h2>
-        <p className="text-sm sm:text-lg lg:w-2/3">{item.description}</p>
-        <button
-          className=" mt-auto w-36 rounded border-2 border-black bg-orange-50 px-4 py-2 text-black hover:bg-green-200 focus:border-blue-300 sm:w-1/2 sm:text-xl md:mt-4"
-          onClick={() => navigate('/mulhousienPizza/pizzas')}
-        >
-          Order Now
-        </button>
+        <p className="text-sm sm:text-lg md:text-right lg:w-2/3">
+          {item.description}
+        </p>{' '}
+        <div className="flex flex-col gap-2 md:w-[80%] md:flex-row">
+          <button
+            className=" mt-auto w-36 rounded  bg-orange-100 px-2 py-2 text-sm text-black hover:bg-orange-300  sm:w-1/2 sm:text-xl md:mt-4"
+            onClick={() => navigate('/mulhousienPizza/pizzas')}
+          >
+            Menu
+          </button>
+          <button
+            className=" mt-auto w-36 rounded  bg-orange-500 px-2 py-2 text-sm text-white hover:bg-orange-300 sm:w-1/2 sm:text-xl md:mt-4"
+            onClick={() => navigate('/mulhousienPizza/pizzas')}
+          >
+            Order Now
+          </button>
+        </div>
       </div>
-      <img
-        src={item.image}
-        className="w-36 rounded sm:w-52 md:w-64 lg:mr-32 lg:w-72"
-        alt="pizza of carousel"
-      />
+      <div className="flex items-center justify-center md:justify-start  ">
+        <img
+          src={item.image}
+          className=" h-40 w-40 sm:h-52 sm:w-52 md:h-64 md:w-64  lg:h-72 lg:w-72"
+          alt="pizza of carousel"
+        />
+      </div>
     </div>
   );
 }
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return <div>Baba black shoe</div>;
 }
 
 export default Footer;
