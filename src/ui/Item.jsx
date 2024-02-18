e3457fad5d3ab3f54f3a6ea397b399028cc4195e
AbdullahPro2 <zeb16208@gmail.com>
2024-02-18 23:44:49 +0100

    Menu COmpleted

---
+++ src/ui/Item.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Item({ item }) {
+  const ingredient = item.ingredients.join(', ');
+  return (
+    <div className="flex flex-col justify-between space-y-2 p-4 ">
+      <img src={item.imageUrl} alt={`${item.name} picture`} />
+      <div className="space-y-1">
+        <h3 className="text-xl">{item.name}</h3>
+        <p className="font-thin italic">{ingredient}</p>
+        <p className="font-bold">${item.unitPrice}</p>
+      </div>
+      <button className="self-baseline bg-orange-400  p-2 text-white hover:bg-orange-300 hover:text-black">
+        ADD TO CART
+      </button>
+    </div>
+  );
+}
+
+export default Item;
--- src/ui/Shop.jsx
+++ src/ui/Shop.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { getMenu } from '../service/apiRestaurant';
 import { useLoaderData } from 'react-router-dom';
+import Item from './Item';
 
 function Shop() {
   const menu = useLoaderData();
   console.log(menu);
-  return <div>Shop</div>;
+  return (
+    <div className="">
+      <h1 className="text-red mt-4 text-center  text-xl font-bold lg:text-3xl">
+        Menu
+      </h1>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+        {menu.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export async function loader() {
