37b8b0aee702df2b243188bb9c09151f0600c8ab
AbdullahPro2 <zeb16208@gmail.com>
2024-02-19 23:53:36 +0100

    Shop completed

--- src/ui/Shop.jsx
+++ src/ui/Shop.jsx
@@ -8,9 +8,7 @@ function Shop() {
   console.log(menu);
   return (
     <div className="">
-      <h1 className="text-red mt-4 text-center  text-xl font-bold lg:text-3xl">
-        Menu
-      </h1>
+      <h1 className=" mt-4 text-center  text-xl font-bold lg:text-3xl">Menu</h1>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {menu.map((item) => (
           <Item item={item} key={item.id} />
